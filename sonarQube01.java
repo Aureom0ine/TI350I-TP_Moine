b6c4509ecb5d3acd47b99764a734f8fa7d4a39d5
Aureom0ine <94388057+Aureom0ine@users.noreply.github.com>
2021-12-03 16:02:42 +0100

    Update sonarQube01.java

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,6 +1,6 @@
 
 public class sonarQube01 {
     int addHeader(int intToAdd, int x, int y){
-        return int.format("Added Header %d %d %d", intToAdd, x, y);
+        return int.format("Added Header %s %d %d", intToAdd, x, y);
     }
 }
